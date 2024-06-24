6f6c47141ce5fa76cafad46a9c84a32df0c0dd6a
chalvinwz <chalvinwz@gmail.com>
2024-06-24 16:07:24 +0700

    init

---
+++ data/siteMetadata.js
@@ -0,0 +1,21 @@
+/** @type {import("pliny/config").PlinyConfig } */
+const siteMetadata = {
+  title: 'Chalvin Wiradhika',
+  author: 'Chalvin Wiradhika',
+  headerTitle: 'Chalvin Wiradhika',
+  description: "Chalvin Wiradhika's Website",
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://chalvinwz.vercel.app',
+  siteRepo: 'https://github.com/chalvinwz/personal-website',
+  siteLogo: '/static/images/logo.png',
+  socialBanner: '/static/images/twitter-card.png',
+  mastodon: 'https://mastodon.social/@mastodonuser',
+  email: 'chalvinwz@gmail.com',
+  github: 'https://github.com/chalvinwz',
+  linkedin: 'https://www.linkedin.com/in/chalvinwiradhika/',
+  instagram: 'https://www.instagram.com/chalvinwz',
+  locale: 'en-US',
+}
+
+module.exports = siteMetadata
